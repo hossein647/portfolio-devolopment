f19099823e531f21a824a65b3bd5f85d4a6a362a
hossein647 <developandroo@gmail.com>
2023-09-02 13:19:15 +0330

    create profile component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
