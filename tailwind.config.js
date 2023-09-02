33092bb23fd20064d15190766aa9f46ab011f664
hossein647 <developandroo@gmail.com>
2023-09-02 23:26:34 +0330

    init cat image in cofig tailwincd

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
   theme: {
     extend: {
       "backgroundImage": {
-        "jungle-img": "url('^assets/img/olivier-guillard-FKJgBUDoVC0-unsplash.jpg')"
+        "jungle-img": "url('^assets/img/olivier-guillard-FKJgBUDoVC0-unsplash.jpg')",
+        "cat-img": "url('^assets/img/kawaiies-plushies-plush-softtoy-blue-gray-shark-cat-dog-pet-bed-house-hideout-pet-beds-blue-medium-891472.jpg')"
       }
     },
   },
