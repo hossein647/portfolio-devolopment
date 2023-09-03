86c433875c72060f5bc9a77c311685181ea3e5a4
hossein647 <developandroo@gmail.com>
2023-09-03 12:10:45 +0330

    add url images of order-food admin-panel github-img magnifying-img information-img

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
     extend: {
       "backgroundImage": {
         "jungle-img": "url('^assets/img/olivier-guillard-FKJgBUDoVC0-unsplash.jpg')",
-        "cat-img": "url('^assets/img/kawaiies-plushies-plush-softtoy-blue-gray-shark-cat-dog-pet-bed-house-hideout-pet-beds-blue-medium-891472.jpg')"
+        "cat-img": "url('^assets/img/kawaiies-plushies-plush-softtoy-blue-gray-shark-cat-dog-pet-bed-house-hideout-pet-beds-blue-medium-891472.jpg')",
+        "order-food-img": "url('^assets/img/order-food.png')",
+        "admin-panel-img": "url('^assets/img/admin-panel.png')",
+        "github-img": "url('^assets/img/github.svg')",
+        "magnifying-img": "url('^assets/img/magnifying-glass.svg')",
+        "information-img": "url('^assets/img/circle-info.svg')",
       }
     },
   },
