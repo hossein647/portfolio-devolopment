30d7f63153291fcbd769933aa64b45b156768335
hossein647 <developandroo@gmail.com>
2023-09-04 17:01:11 +0330

    add event phoneClicked for copy phone number

--- src/app/home/profile/profile.component.ts
+++ src/app/home/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  phoneClicked(phone: HTMLImageElement) {
+    const phoneIcon = phone.src.includes('phone');
+    if (phoneIcon) phone.src = './../../../assets/img/copy-regular.svg';
+    else {
+      phone.src = './../../../assets/img/phone-solid.svg';
+      navigator.clipboard.writeText('09149584922');
+    }
+  }
 }
