5cf8cd58137cc7743c692273f29b48b2161dfbfa
hossein647 <developandroo@gmail.com>
2023-09-05 17:37:14 +0330

    define product input and implement OnInit hook

--- src/app/details-product/details-product.component.ts
+++ src/app/details-product/details-product.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../_interfaces/prodoct.interface';
 
 @Component({
   selector: 'app-details-product',
   templateUrl: './details-product.component.html',
   styleUrls: ['./details-product.component.scss']
 })
-export class DetailsProductComponent {
+export class DetailsProductComponent implements OnInit {
 
+  @Input({ required: true }) product: Product;
+
+
+  ngOnInit() {
+
+  }
 }
