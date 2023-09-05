ca358334b7ee9c6f3cdb54b42a0e210a6c289534
hossein647 <developandroo@gmail.com>
2023-09-05 17:11:39 +0330

    import HomeComponent ProfileComponent ProductsComponent DetailsProductComponent to AppModule and remove HomeModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DetailsProductComponent } from './details-product/details-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ProductsComponent,
+    DetailsProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
