cee6f4ccc0720667b0fbda4ba52d8743ec54fc6e
hossein647 <developandroo@gmail.com>
2023-09-07 01:03:49 +0330

    responsive HomeComponent and add arrow icons for smooth scroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 import { Product } from '../_interfaces/prodoct.interface';
 
 
@@ -11,18 +11,41 @@ export class HomeComponent {
 
   showDetailsProduct = false;
   product: Product;
+  scrollHeight: number;
+  showArrowBottom: boolean;
   
   constructor() { }
 
 
   ngOnInit(): void {
-    
+    console.log(document.documentElement.scrollTop);
+    console.log(window.pageYOffset, window.screenY, document.body.scrollHeight, document.documentElement.scrollHeight);
+    this.scrollHeight = document.body.scrollHeight || document.documentElement.scrollHeight
+    if (window.pageXOffset > 50 || window.scrollY > 50) this.showArrowBottom = false;
+    else this.showArrowBottom = true;
   }
 
 
   onShowDetails(product: Product) {
     this.product = product;
-    this.showDetailsProduct = true
+    this.showDetailsProduct = true;
+    document.body.style.overflowY = 'hidden';
   }
   
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll(event: any) {
+    console.log(window.scrollY);
+    if (window.pageXOffset > 50 || window.scrollY > 50) {
+      this.showArrowBottom = false;
+    } else {
+      this.showArrowBottom = true;
+    }
+  }
+
+
+  gotToPositonScroll() {
+    if (this.showArrowBottom) window.scrollTo(0, this.scrollHeight);
+    else window.scrollTo(0, 0);
+  }
 }
