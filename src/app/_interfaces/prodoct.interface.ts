c336c951750866db38726c7927775498fe9b2620
hossein647 <developandroo@gmail.com>
2023-09-07 15:25:36 +0330

    add shortDescription && demo && github properties to Product interface

--- src/app/_interfaces/prodoct.interface.ts
+++ src/app/_interfaces/prodoct.interface.ts
@@ -2,6 +2,9 @@ export interface Product {
     title: string; 
     color: string;
     years: string;
+    shortDescription: string;
+    demo: string;
+    github: string;
     src: string;
     feature: string[];
     technology: { front: string[], back: string[] };
